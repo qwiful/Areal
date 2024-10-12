592d21e75346e8277a16782569044055b7c10596
qwiful <161530951+qwiful@users.noreply.github.com>
2024-10-12 20:36:00 +0300

    фильтр по отделу и должности
    Добавила ещё одного сотрудника в базу sotrudniki.php. Сделала фильтр по отелу и должности index.php, добавила ещё стилей style.css. Фильтр работает, но есть проблемы: поля не очищаются после нажатия на кнопку (остаются записи, которые вносили для фильтрации). это неудобно, так как для вывода всех данных приходится очищать оба поля и снова нажимать на кнопку. пыталась сделать кнопку "обновить", чтобы она очищала поля и выводила изначальную таблицу, но не получилось.

--- index.php
+++ index.php
@@ -1,61 +1,84 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet" type="text/css">
-    <title>Учёт сотрудников</title>
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <title>Учёт сотрудников</title>
+    </head>
 
-<body> 
-<?php
-require_once 'config.php';
+    <body> 
+    <?php
+    require_once 'config.php';
 
-$query = "SELECT * FROM sotr";
-$result = mysqli_query($conn, $query);
+    //запрос для фильта по отделу и должности
+    $FOtdel = isset($_GET['FOtdel']) ?$_GET['FOtdel'] : '';
+    $FDolzhnost = isset($_GET['FDolzhnost']) ? $_GET['FDolzhnost'] : '';
 
-if (!$result) {
-    die("Ошибка запроса: " . mysqli_error($conn));
-}
- 
-$list = array();
-while ($row = mysqli_fetch_assoc($result)) {
+    $query = "SELECT * FROM sotr WHERE 1=1";
+    if ($FOtdel) {
+        $query .= " AND Otdel LIKE '%$FOtdel%'";
+    }
+    if ($FDolzhnost) {
+        $query .= " AND Dolzhnost LIKE '%$FDolzhnost%'";
+    }
+
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        die("Ошибка запроса: " . mysqli_error($conn));
+    }
+
+    $list = array();
+    while ($row = mysqli_fetch_assoc($result)) {
     $list[] = $row;
-}
-?>
-<h1>Сотрудники организации</h1>
-<table>
-<thead>
-    <th style=width:5%>ID</th>
-    <th style=width:15%;>ФИО</th>
-    <th>Дата рождения</th>
-    <th>Паспорт</th>
-    <th>Контактная информация</th>
-    <th style=width:15%;>Адрес</th>
-    <th>Отдел</th>
-    <th>Должность</th>
-    <th>Зарплата</th>
-    <th>Дата приема на работу</th>
-    <th>Статус работы</th>
-</thead>
-<tbody>
-    <?php foreach($list as $item): ?>
-        <tr> 
-            <td> <?php echo $item['ID'];?></td>
-            <td> <?php echo $item['FIO'];?></td>
-            <td> <?php echo $item['DataRozhdenia'];?></td>
-            <td> <?php echo $item['Pasport'];?></td>
-            <td> <?php echo $item['KontaktnayaInfa'];?></td>
-            <td> <?php echo $item['Adres'];?></td>
-            <td> <?php echo $item['Otdel'];?></td>
-            <td> <?php echo $item['Dolzhnost'];?></td>
-            <td> <?php echo $item['Zarplata'];?></td>
-            <td> <?php echo $item['DataPrinatia'];?></td>
-            <td> <?php echo $item['Status'];?></td>
-        </tr>
-    <?php endforeach; ?>
-</tbody>
-</table>
-</body>
-</html>    
+    }
+
+    ?>
+    <h1>Сотрудники организации</h1>
+    <table>
+    <thead>
+        <th style=width:5%>ID</th>
+        <th style=width:15%;>ФИО</th>
+        <th>Дата рождения</th>
+        <th>Паспорт</th>
+        <th>Контактная информация</th>
+        <th style=width:15%;>Адрес</th>
+        <th>Отдел</th>
+        <th>Должность</th>
+        <th>Зарплата</th>
+        <th>Дата приема на работу</th>
+        <th>Статус работы</th>
+    </thead>
+    <tbody>
+        <?php foreach($list as $item): ?>
+            <tr> 
+                <td> <?php echo htmlspecialchars($item['ID']);?></td>
+                <td> <?php echo htmlspecialchars($item['FIO']);?></td>
+                <td> <?php echo htmlspecialchars($item['DataRozhdenia']);?></td>
+                <td> <?php echo htmlspecialchars($item['Pasport']);?></td>
+                <td> <?php echo htmlspecialchars($item['KontaktnayaInfa']);?></td>
+                <td> <?php echo htmlspecialchars($item['Adres']);?></td>
+                <td> <?php echo htmlspecialchars($item['Otdel']);?></td>
+                <td> <?php echo htmlspecialchars($item['Dolzhnost']);?></td>
+                <td> <?php echo htmlspecialchars($item['Zarplata']);?></td>
+                <td> <?php echo htmlspecialchars($item['DataPrinatia']);?></td>
+                <td> <?php echo htmlspecialchars($item['Status']);?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+    </table>
+    <h3> Фильтрация по должности или отделу: </h3>
+    <form class = "filtr" method="GET" action="">
+
+    <label   class = "filtr" for="Otdel">Отдел:</label>
+    <input type="text" id="Otdel" name="FOtdel" value="<?php echo htmlspecialchars($FOtdel); ?>">
+
+    <label  class = "filtr" for="Dolzhnost">Должность:</label>
+    <input type="text" id="Dolzhnost" name="FDolzhnost" value="<?php echo htmlspecialchars($FDolzhnost); ?>">
+
+    <button type="submit">Найти</button>
+    </form>
+    </body>
+    </html>    
--- sotrudniki.sql
+++ sotrudniki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 10 2024 г., 21:30
+-- Время создания: Окт 12 2024 г., 19:23
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -31,21 +31,23 @@ CREATE TABLE `sotr` (
   `ID` int(11) NOT NULL,
   `FIO` varchar(75) NOT NULL,
   `DataRozhdenia` date NOT NULL,
-  `Pasport` varchar(10) NOT NULL,
+  `Pasport` varchar(15) NOT NULL,
   `KontaktnayaInfa` varchar(75) NOT NULL,
   `Adres` varchar(75) NOT NULL,
   `Otdel` varchar(75) NOT NULL,
   `Dolzhnost` varchar(75) NOT NULL,
   `Zarplata` int(11) NOT NULL,
-  `DataPrinatia` date NOT NULL
+  `DataPrinatia` date NOT NULL,
+  `Status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `sotr`
 --
 
-INSERT INTO `sotr` (`ID`, `FIO`, `DataRozhdenia`, `Pasport`, `KontaktnayaInfa`, `Adres`, `Otdel`, `Dolzhnost`, `Zarplata`, `DataPrinatia`) VALUES
-(1, 'Шмелькова Анастасия Ивановна', '1985-10-09', ' 980594179', 'Nastya_Shmel@gmail.com', 'г.Ярославль, ул.Блюхера, д.76, кв.34', 'Бухгалтерия', 'Главный бухгалтер', 85000, '2018-02-02');
+INSERT INTO `sotr` (`ID`, `FIO`, `DataRozhdenia`, `Pasport`, `KontaktnayaInfa`, `Adres`, `Otdel`, `Dolzhnost`, `Zarplata`, `DataPrinatia`, `Status`) VALUES
+(1, 'Шмелькова Анастасия Ивановна', '1985-10-09', ' 9805 941745', '+7(921)820-80-15', 'ул.Блюхера, д.76', 'Бухгалтерия', 'Главный бухгалтер', 85000, '2018-02-02', 'Работает'),
+(2, 'Попов Сергей Витальевич', '1990-04-08', ' 8954 975845', '+7(921)675-88-44', 'ул.Новостройская д.17', 'Маркетинг', 'Начальник отдела', 96000, '2018-07-24', 'Работает');
 
 --
 -- Индексы сохранённых таблиц
@@ -56,6 +58,16 @@ INSERT INTO `sotr` (`ID`, `FIO`, `DataRozhdenia`, `Pasport`, `KontaktnayaInfa`, 
 --
 ALTER TABLE `sotr`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `sotr`
+--
+ALTER TABLE `sotr`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
